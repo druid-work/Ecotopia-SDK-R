07d0e108c474dda5621436a73a0ee858b4514520
yinear <yinear@outlook.com>
2024-05-07 14:57:29 +0800

    chrone: replace EcotopiaTools with Ecotopia-SDK-R

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(EcotopiaTools)
+library(Ecotopia-SDK-R)
 
-test_check("EcotopiaTools")
+test_check("Ecotopia-SDK-R")
